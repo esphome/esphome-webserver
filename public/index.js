4a02a4d07dbe3aceb480378e92dd1164c9690efd
wilberforce <wilber@wilberforce.co.nz>
2021-10-22 07:34:25 +1300

    smaller build

--- public/index.js
+++ public/index.js
@@ -1,11 +1,18 @@
+
+import { render } from 'preact';
+function App() {
+    return <h1>Hello World!</h1>;
+}
+
+render(<App />, document.body);
+/*
 import { LocationProvider, Router, Route, lazy, ErrorBoundary, hydrate, prerender as ssr } from 'preact-iso';
 import Home from './pages/home/index.js';
 import NotFound from './pages/_404.js';
 import Header from './header.js';
+import About from './pages/about/index.js';
 
-const About = lazy(() => import('./pages/about/index.js'));
-
-export function App() {
+function App() {
 	return (
 		<LocationProvider>
 			<div class="app">
@@ -27,3 +34,4 @@ hydrate(<App />);
 export async function prerender(data) {
 	return await ssr(<App {...data} />);
 }
+*/
--- wmr.config.mjs
+++ wmr.config.mjs
@@ -1,26 +1,18 @@
 import { defineConfig } from "wmr";
 
+/*
+import nomodule from '@wmrjs/nomodule';
+
+export function build(config) {
+	nomodule(config);
+}
+*/
+
 // Full list of options: https://wmr.dev/docs/configuration
 export default defineConfig({
   /* Your configuration here */
   alias: {
     react: "preact/compat",
     "react-dom": "preact/compat",
-  },
-  build: {
-    brotliSize: false,
-    cssCodeSplit: true,
-    cleanCssOptions: {
-      level: 2,
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: () => "everything.js", // create one js bundle
-      },
-    },
-  },
-  server: {
-    open: "/", // auto open browser in dev mode
-    host: true, // and on dev ip
-  },
+  }
 });
