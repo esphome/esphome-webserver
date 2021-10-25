c78692eb887099a272a36d992eec0c81b12b7831
wilberforce <wilber@wilberforce.co.nz>
2021-10-25 15:55:19 +1300

    proxy working

--- public/webserver-v2.js
+++ public/webserver-v2.js
@@ -1,5 +1,10 @@
-import { render } from "preact";
-import { html, Component } from "htm/preact";
+import {
+  render
+} from "preact";
+import {
+  html,
+  Component
+} from "htm/preact";
 
 // C:\Users\rhys\source\repos\wmr\examples\demo\public\pages\meta-tags.js
 // C:\Users\rhys\source\repos\wmr\examples\demo\public\pages\json.js
@@ -8,66 +13,85 @@ class App extends Component {
   constructor() {
     super();
     const source = new EventSource("/events");
-    
+
     const entityByid = entities.reduce((map, entity) => {
       map[`${entity.entity}-${entity.id}`] = entity;
       return map;
     }, {});
-  
+
     source.addEventListener("state", function (e) {
       const data = JSON.parse(e.data);
-      let ref=entityByid[data.id];
-      if ( ref ) {
-        ref.state=data.state;
-        ref.value=data.value;
+      let ref = entityByid[data.id];
+      if (ref) {
+        ref.state = data.state;
+        ref.value = data.value;
       } else {
         // Dynamically add discovered..
         console.log(`discovered:${data.id}`)
-        let parts=data.id.split('-')
+        let parts = data.id.split('-')
         let entity = {
           entity: parts[0],
           id: parts[1],
           state: data.state,
-          value: data. value,
+          value: data.value,
           name: data.id
         };
         entities.push(entity);
-        entityByid[data.id]=entity;
+        entityByid[data.id] = entity;
       }
     });
     source.addEventListener("log", (e) => {
-      let parts=e.data.slice(10,e.data.length-4).split(':');
-      const record =  {
-        sort: e.data.slice(0,7),
-        level: e.data.slice(7,4),
+      let parts = e.data.slice(10, e.data.length - 4).split(':');
+      const debug = {
+        "[1;31m": 'e',
+        "[0;33m": 'w',
+        "[0;32m": 'i',
+        "[0;35m": 'c',
+        "[0;36m": 'd',
+        "[0;37m": 'v'
+      };
+      const record = {
+        sort: debug[e.data.slice(0, 7)],
+        level: e.data.slice(7, 10),
         who: `${parts[0]}:${parts[1]}`,
         detail: parts[2],
         when: (new Date).toTimeString().split(' ')[0]
-    };
+      };
       this.addLog(record);
     });
   }
 
   addLog(log) {
-    const { logs = [] } = this.state;
+    const {
+      logs = []
+    } = this.state;
 
     logs.unshift(log);
-    this.setState({ logs: logs });
+    this.setState({
+      logs: logs
+    });
   }
 
   toggle(entity) {
-    fetch(`/${entity.entity}/${entity.id}/toggle`, { method: "POST", body: 'true' }).then((r) => {
+    fetch(`/${entity.entity}/${entity.id}/toggle`, {
+      method: "POST",
+      body: 'true'
+    }).then((r) => {
       console.log(r);
     });
   }
 
-  render({ page }, { logs = [] }) {
-    return html`
+  render({
+    page
+  }, {
+    logs = []
+  }) {
+    return html `
       <article>
         <h1>${document.title}</h1>
         <h2>States</h2>
 
-        <table>
+        <table class="pure-table">
           <thead>
             <tr>
               <th>Name</th>
@@ -82,25 +106,24 @@ class App extends Component {
                   <tr>
                     <td>${entity.name}</td>
                     <td>${entity.state}</td>
-                    <td><button onClick=${() => this.toggle(entity)}>Toggle</button></td>
+                    <td><button class="pure-button pure-button-primary" onClick=${() => this.toggle(entity)}>Toggle</button></td>
                   </tr>
                 `
             )}
           </tbody>
         </table>
 
-        <table>
+        <table id="log" class="pure-table" style="font-family: monospace,monospace;">
           <thead>
             <tr>
               <th>Time</th>
-              <th>sort</th>
               <th>level</th>
               <th>who</th>
               <th style="width:50%">detail</th>
             </tr>
             </thead>
           <tbody>
-          ${logs.map((log) => html` <tr><td>${log.when}</td><td>${log.sort}</td><td>${log.level}</td><td>${log.who}</td><td>${log.detail}</td></tr> `)}
+          ${logs.map((log) => html` <tr class="${log.sort}"><td>${log.when}</td><td>${log.level}</td><td>${log.who}</td><td>${log.detail}</td></tr> `)}
           </tbody>
             </table>
 
@@ -109,32 +132,10 @@ class App extends Component {
           <input type="file" name="update" />
           <input type="submit" value="Update" />
         </form>
+</div>        
 
       </article>
     `;
   }
 }
-/*
-source.addEventListener("log", function (e) {
-  const log = document.getElementById("log");
-  let klass = "";
-  if (e.data.startsWith("[1;31m")) {
-    klass = "e";
-  } else if (e.data.startsWith("[0;33m")) {
-    klass = "w";
-  } else if (e.data.startsWith("[0;32m")) {
-    klass = "i";
-  } else if (e.data.startsWith("[0;35m")) {
-    klass = "c";
-  } else if (e.data.startsWith("[0;36m")) {
-    klass = "d";
-  } else if (e.data.startsWith("[0;37m")) {
-    klass = "v";
-  } else {
-    log.innerHTML += e.data + "\n";
-  }
-  log.innerHTML += '<span class="' + klass + '">' + e.data.substr(7, e.data.length - 10) + "</span>\n";
-});
-*/
-
-render(html`<${App} page="All" />`, document.body);
+render(html `<${App} page="All" />`, document.body);
--- wmr.config.mjs
+++ wmr.config.mjs
@@ -1,45 +1,33 @@
 import { defineConfig } from "wmr";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
-import zip from "rollup-plugin-zip";
-
-// Full list of options: https://wmr.dev/docs/configuration
-// middleware https://github.com/preactjs/wmr/issues/297
-
 const proxy_target = "http://nodemcu.local";
 export default defineConfig((options) => {
-  const proxy_events = createProxyMiddleware('/events',{
-    //changeOrigin: true,
+  const proxy_events = createProxyMiddleware("/events", {
     logLevel: "debug",
     target: `${proxy_target}`,
   });
   const proxy_post = createProxyMiddleware({
     changeOrigin: true,
     logLevel: "debug",
-    target: `${proxy_target}`, // /switch/relay/toggle
+    target: `${proxy_target}`,
   });
   options.middleware.push((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    // developement: redirect _static folder
+    if (options.mode === "start" && req.path.startsWith("/_static")) {
+      res.writeHead(302, { Location: req.path.replace("/_static", ""), "Access-Control-Allow-Origin": "*" });
+      return res.end("");
+    }
     if (req.method === "POST") {
-      console.dir(req.path)
       proxy_post(req, res, next);
       return;
-    }
-    if ( options.mode === "start" ) {
-      if ( req.path === '/_static/webserver-v2.js') 
-      { console.log('js!');
-      req.path === '/public/webserver-v2.js';
-    }
-      if ( req.path === '/_static/webserver-v2.css') req.path === '/public/webserver-v2.css';
     }
     if (req.path.match(/^\/events(|$)/)) proxy_events(req, res, next);
     else next();
   });
   return {
-    output: { chunkFileNames: '[name].js',
-     assetFileNames: '[name][extname]',
-     dir:'dist/_static'
-    },
+    output: { chunkFileNames: "[name].js", assetFileNames: "[name][extname]", dir: "dist/_static" },
     host: "0.0.0.0",
     port: 80,
     //debug: true,
