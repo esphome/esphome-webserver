f97f2a1321e47ab5f819e71508c95e8abd8cfa2d
wilberforce <wilber@wilberforce.co.nz>
2021-11-09 17:46:34 +1300

    basic hover states

--- src/css/button.ts
+++ src/css/button.ts
@@ -10,8 +10,20 @@ export default css`
     color: inherit;
     border: 1px solid rgba(127, 127, 127, 0.5);
     height: 1.2rem;
-    padding-left: 0.25rem;padding-right: 0.25rem;
+    padding-left: 0.25rem;
+    padding-right: 0.25rem;
+  }
+
+  button:active,
+  .btn:active {
+    background-image: linear-gradient(0deg, rgba(127, 127, 127, 0.8) 0%, rgba(127, 127, 127, 0.2) 100%);
+    transition-duration: 1s
+  }
+
+  button:hover,
+  .btn:hover {
+    background-image: linear-gradient(0deg, rgba(127, 127, 127, 0.2) 0%, rgba(127, 127, 127, 0.8) 100%);
+    transition-duration: 1s
   }
 `;
 
-// To do hover and click states
