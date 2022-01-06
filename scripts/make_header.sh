f3adcb08f09b6437ddf1ebda791cc1ea6dc2f5b0
wilberforce <wilber@wilberforce.co.nz>
2022-01-06 20:19:23 +1300

    add namespaces

--- scripts/make_header.sh
+++ scripts/make_header.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cat <<EOT > ./$1/$2
 #pragma once
-// Generated from https://github.com/esphome/esphome-webserver/tree/main/captive-portal
+// Generated from https://github.com/esphome/esphome-webserver
 namespace esphome {
 
-namespace captive_portal {
+namespace $3 {
 
 EOT
 echo "const uint8_t INDEX_GZ[] PROGMEM = {" >> ./$1/$2
 xxd -cols 19 -i $1/index.html.gz | sed -e '2,$!d' -e 's/^/  /' -e '$d' | sed  -e '$d' | sed -e '$s/$/};/' >> ./$1/$2
 cat <<EOT >> ./$1/$2
 
-}  // namespace captive_portal
+}  // namespace $3
 }  // namespace esphome
 EOT
 ls -l ./$1/$2
