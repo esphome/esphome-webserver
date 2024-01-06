f1c34fe5ce3d60c6f927e0a7584b9e380c0c4d4f
Darter <rfdarter@googlemail.com>
2024-01-06 12:53:46 +0100

    changed back to width: calc(100% - 8rem); but added a min-width so it show correcly on PC but also is visible on mobile devices

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import copy from "rollup-plugin-copy";
 import stripBanner from "rollup-plugin-strip-banner";
 import replace from "@rollup/plugin-replace";
 
-const proxy_target = "http://nodemcu.local";
+const proxy_target = "http://chicken-coop-light.local";
 
 export default defineConfig({
   clearScreen: false,
