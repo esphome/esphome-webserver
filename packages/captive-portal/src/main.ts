3ebbe38f558e85d5e9baaefabb8c5b854451b4dc
J. Nick Koston <nick@home-assistant.io>
2026-01-09 13:35:20 -1000

    must be new window

--- packages/captive-portal/src/main.ts
+++ packages/captive-portal/src/main.ts
@@ -34,7 +34,7 @@ fetch("/config.json").then(function (response) {
     document.querySelector("link[rel~='icon']").href = `data:image/svg+xml,${wifi(-65)}`;
     if (config.web_server) {
       let webServerLink = document.createElement("div");
-      webServerLink.innerHTML = `<h3>Device Control</h3><a href="/?web_server=1">Open Web Server</a><br><br><hr><br>`;
+      webServerLink.innerHTML = `<h3>Device Control</h3><a href="/?web_server=1" target="_blank">Open Web Server</a><br><br><hr><br>`;
       // Insert before OTA Update section (third h1)
       document.body.getElementsByTagName("h1")[2].before(webServerLink);
     }
