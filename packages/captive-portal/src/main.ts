00c323cfa4a46a5160d388a32e71e421b220094b
J. Nick Koston <nick@home-assistant.io>
2026-01-09 11:56:52 -1000

    fxies

--- packages/captive-portal/src/main.ts
+++ packages/captive-portal/src/main.ts
@@ -34,8 +34,9 @@ fetch("/config.json").then(function (response) {
     document.querySelector("link[rel~='icon']").href = `data:image/svg+xml,${wifi(-65)}`;
     if (config.web_server) {
       let webServerLink = document.createElement("div");
-      webServerLink.innerHTML = `<br><hr><br><h3>Device Control</h3><a href="/?web_server">Open Web Server</a>`;
-      document.querySelector("#net").after(webServerLink);
+      webServerLink.innerHTML = `<h3>Device Control</h3><a href="/?web_server">Open Web Server</a><br><br><hr><br>`;
+      // Insert before OTA Update section (third h1)
+      document.body.getElementsByTagName("h1")[2].before(webServerLink);
     }
   })
 })
