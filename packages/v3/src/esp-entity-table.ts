b65b042b41c47c5c8c6c75e2773571939457cfcd
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2026-07-27 11:29:24 +1200

    Update packages/v3/src/esp-entity-table.ts

--- packages/v3/src/esp-entity-table.ts
+++ packages/v3/src/esp-entity-table.ts
@@ -718,13 +718,15 @@ class ActionRenderer {
 
   render_cover() {
     if (!this.entity) return;
-    if (this.entity.assumed_state)
+    if (this.entity.assumed_state) {
       return html`${this._actionButton(this.entity, "↑", "open")}
       ${this._actionButton(this.entity, "☐", "stop")}
       ${this._actionButton(this.entity, "↓", "close")}`;
-    else return html`${this._actionButton(this.entity, "↑", "open", this.entity.state === "OPEN")}
-    ${this._actionButton(this.entity, "☐", "stop")}
-    ${this._actionButton(this.entity, "↓", "close", this.entity.state === "CLOSED")}`;
+    } else {
+      return html`${this._actionButton(this.entity, "↑", "open", this.entity.state === "OPEN")}
+      ${this._actionButton(this.entity, "☐", "stop")}
+      ${this._actionButton(this.entity, "↓", "close", this.entity.state === "CLOSED")}`;
+    }
   }
 
   render_button() {
